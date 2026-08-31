63c712c012d8dd9eb14a0d23dc538f23486d026a
Lakshyaonlinezone <sk9008615@gmail.com>
2026-09-01 00:04:17 +0530

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,14 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Lakshya World",
+  description: "Movies, Music and Entertainment"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="hi">
+      <body>{children}</body>
+    </html>
+  );
+}
